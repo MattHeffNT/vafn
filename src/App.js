040acb05a35625484d2763745b4418fb04945372
Matt <heffa2004@gmail.com>
2021-04-30 01:02:08 +1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import BarcodeScannerComponent from "react-webcam-barcode-scanner";
+ 
+function App() {
+ 
+  const [ data, setData ] = React.useState('Not Found');
+ 
+  return (
+    <>
+      <BarcodeScannerComponent
+        width={500}
+        height={500}
+        onUpdate={(err, result) => {
+          if (result) setData(result.text)
+          else setData('Not Found')
+        }}
+      />
+      <p>{data}</p>
+    </>
+  )
+}
+ 
+export default App;
