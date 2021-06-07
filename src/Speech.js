6f7c217b594a2155c9ed529b4d2d63d4a2103d2d
Matt <heffa2004@gmail.com>
2021-06-07 23:39:54 +1000

    test

--- src/Speech.js
+++ src/Speech.js
@@ -1,5 +1,5 @@
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
@@ -10,10 +10,19 @@ import {
   Route,
   Link
 } from "react-router-dom";
+// import Sent from './sent';
+
+import Modal  from 'react-bootstrap/Modal';
+import ModalHeader  from 'react-bootstrap/ModalHeader';
+import  ModalBody  from 'react-bootstrap/ModalBody';
+import  ModalFooter from 'react-bootstrap/ModalFooter';
+import  Button from 'react-bootstrap/Button';
+import Sent from './sent';
+
 
 function Dictaphone ()  {
 
-    const { finalTranscript, resetTranscript } = useSpeechRecognition()
+    const { transcript, resetTranscript } = useSpeechRecognition()
 
     // Update the document title using the browser API
     useEffect(()=>{
@@ -24,7 +33,11 @@ function Dictaphone ()  {
         startRecording()
     })
 
-    // console.log(finalTranscript)
+    // Modal variables
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     let stopTranscript = () => {
       SpeechRecognition.stopListening()
@@ -35,14 +48,17 @@ function Dictaphone ()  {
       return null
     }
 
-    if (finalTranscript === "OK Google test") {
+    if (transcript === "OK Google test") {
 
         let activate = () => {
-          console.log (finalTranscript)
-
-          
+          console.log (transcript)
         }
+
+        
+        handleShow();
         activate();
+
+        // call modal
     }
     
     return (
@@ -56,9 +72,9 @@ function Dictaphone ()  {
 
           <Jumbotron>
             <h1>Voice Activated Family Notification</h1>
-            {finalTranscript}
           </Jumbotron>
 
+          <Sent></Sent>
           {/* <div className="transcript-cont">{transcript}</div> */}
         </Container>
   
