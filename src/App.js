4d745c2b7c56a172729bcbb4a40289fbf2baedea
Matt <heffa2004@gmail.com>
2022-05-04 02:49:50 +0930

    remove old node version and build cmd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-
 import Dictaphone from './Speech.js';
 import Submit from './Submit';
 import Landing from './landing';
 import Contact from './contact';
-
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -16,11 +14,8 @@ function App() {
                     <Route exact path="/" component={Landing}>
                         <Landing />
                     </Route>
-
                     <Route path="/contact" component={Contact}></Route>
-
                     <Route path="/submit" component={Submit}></Route>
-
                     <Route path="/speech" component={Dictaphone}>
                         <Dictaphone />
                     </Route>
