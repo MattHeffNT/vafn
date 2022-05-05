5e2f227541f28e7bc894447ecd42cf2cc353a5cd
MattHeffNT <heffa2004@gmail.com>
2022-05-05 17:10:53 +0930

    test comment

--- src/App.js
+++ src/App.js
@@ -5,24 +5,26 @@ import Landing from './landing';
 import Contact from './contact';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
+// test comment 
+
 function App() {
-    return (
-        <div className="component">
-            {/* controller for front end */}
-            <Router>
-                <Switch>
-                    <Route exact path="/" component={Landing}>
-                        <Landing />
-                    </Route>
-                    <Route path="/contact" component={Contact}></Route>
-                    <Route path="/submit" component={Submit}></Route>
-                    <Route path="/speech" component={Dictaphone}>
-                        <Dictaphone />
-                    </Route>
-                </Switch>
-            </Router>
-        </div>
-    );
+  return (
+    <div className="component">
+      {/* controller for front end */}
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Landing}>
+            <Landing />
+          </Route>
+          <Route path="/contact" component={Contact}></Route>
+          <Route path="/submit" component={Submit}></Route>
+          <Route path="/speech" component={Dictaphone}>
+            <Dictaphone />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
